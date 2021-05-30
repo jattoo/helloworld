99e84a7028ff71a4734cadfba302114184fb9058
jattoo <m_krub@aol.com>
2021-05-30 21:06:34 +0300

    modified base.py file again to trigger code deployments

--- helloworld/helloworld/settings/base.py
+++ helloworld/helloworld/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-#the main setting file for the project
+#the main setting file for the project again
 
 import environ
 import os
