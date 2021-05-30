8a31d2f4033be9504278f959898779e98f96701e
jattoo <m_krub@aol.com>
2021-05-30 21:03:31 +0300

    modified base.py file

--- helloworld/helloworld/settings/base.py
+++ helloworld/helloworld/settings/base.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+#the main setting file for the project
+
 import environ
 import os
 from pathlib import Path
